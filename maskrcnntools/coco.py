0ab81f9915f635148b90c2a11faf830296014d0d
alexeybokhovkin <bohovkinalex@gmail.com>
2019-08-16 17:49:15 +0300

    working maskrcnn

--- maskrcnntools/coco.py
+++ maskrcnntools/coco.py
@@ -4,7 +4,7 @@ from copy import deepcopy
 
 import torch
 from pycocotools.coco import COCO
-from torchvision.datasets.vision import  VisionDataset
+from torchvision.datasets.vision import VisionDataset
 from PIL import Image
 
 
@@ -61,3 +61,53 @@ class CocoDetection(VisionDataset):
 
     def __len__(self):
         return len(self.ids)
+
+    
+class CocoDataset(object):
+    def __init__(self, torch_dataset, transforms=None):
+        self.torch_dataset = torch_dataset
+        self.transforms = transforms
+        
+    def __getitem__(self, idx):
+        img, target = self.torch_dataset[idx]
+        
+        masks = annsToSeg(target, self.torch_dataset.coco)[3]
+        masks = masks.astype(bool)
+        obj_ids = [x['category_id'] for x in target]
+        
+        num_objs = len(obj_ids)
+        boxes = []
+        for i in range(num_objs):
+            pos = np.where(masks[i])
+            xmin = np.min(pos[1])
+            xmax = np.max(pos[1])
+            ymin = np.min(pos[0])
+            ymax = np.max(pos[0])
+            boxes.append([xmin, ymin, xmax, ymax])
+            
+        boxes = torch.as_tensor(boxes, dtype=torch.float32)
+        labels = [x['category_id'] for x in target]
+        labels = torch.as_tensor(labels, dtype=torch.int64)
+        masks = torch.as_tensor(masks, dtype=torch.uint8)
+        
+        image_id = torch.tensor([idx])
+        area = (boxes[:, 3] - boxes[:, 1]) * (boxes[:, 2] - boxes[:, 0])
+        
+        iscrowd = [x['iscrowd'] for x in target]
+        iscrowd = torch.as_tensor(iscrowd, dtype=torch.int64)
+        
+        target = {}
+        target["boxes"] = boxes
+        target["labels"] = labels
+        target["masks"] = masks
+        target["image_id"] = image_id
+        target["area"] = area
+        target["iscrowd"] = iscrowd
+
+        if self.transforms is not None:
+            img, target = self.transforms(img, target)
+
+        return img, target
+    
+    def __len__(self):
+        return len(self.torch_dataset)
