612985406e61f0904ff5859b87c7050f733ec5d3
Ivan Skorokhodov <iskorokhodov@gmail.com>
2019-08-21 10:18:37 +0300

    fix: wrong indexing in loss computation

--- src/trainers/densepose_trainer.py
+++ src/trainers/densepose_trainer.py
@@ -54,18 +54,18 @@ class DensePoseRCNNTrainer(BaseTrainer):
                 'coco_anns': target,
             }
 
-        # train_ds = CocoDetection(f'{self.config.data.coco_dir}/train2014',
-        #                          f'{self.config.data.annotations_dir}/densepose_coco_2014_train.json',
-        #                          transform=transforms.ToTensor(),
-        #                          target_transform=torchvision_target_format)
+        train_ds = CocoDetection(f'{self.config.data.coco_dir}/train2014',
+                                 f'{self.config.data.annotations_dir}/densepose_coco_2014_train.json',
+                                 transform=transforms.ToTensor(),
+                                 target_transform=torchvision_target_format)
         val_ds = CocoDetection(f'{self.config.data.coco_dir}/val2014',
                                  f'{self.config.data.annotations_dir}/densepose_coco_2014_minival.json',
                                  transform=transforms.ToTensor(),
                                  target_transform=torchvision_target_format)
-        train_ds = CocoDetection(f'{self.config.data.coco_dir}/train2014',
-                               f'{self.config.data.annotations_dir}/densepose_coco_2014_tiny.json',
-                               transform=transforms.ToTensor(),
-                               target_transform=torchvision_target_format)
+        # train_ds = CocoDetection(f'{self.config.data.coco_dir}/train2014',
+        #                        f'{self.config.data.annotations_dir}/densepose_coco_2014_tiny.json',
+        #                        transform=transforms.ToTensor(),
+        #                        target_transform=torchvision_target_format)
         # val_ds = CocoDetection(f'{self.config.data.coco_dir}/train2014',
         #                          f'{self.config.data.annotations_dir}/densepose_coco_2014_tiny.json',
         #                          transform=transforms.ToTensor(),
